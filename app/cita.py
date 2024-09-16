eb9021694de39b5d6f078fb3ada23ec9cb2b6eae
santrix-rc <108999410+santrix-rc@users.noreply.github.com>
2024-09-16 17:43:59 -0500

    Módulo que define la clase Cita

--- app/cita.py
+++ app/cita.py
@@ -1,1 +1,10 @@
+# Autor: Santiago
+class Cita:
+    def __init__(self, paciente, medico, fecha, hora):
+        self.paciente = paciente
+        self.medico = medico
+        self.fecha = fecha
+        self.hora = hora
 
+    def __str__(self):
+        return f"{self.paciente} con {self.medico} el {self.fecha} a las {self.hora}"
