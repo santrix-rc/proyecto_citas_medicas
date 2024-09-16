1e979586a197edd140dabec65e11ad387826fc1c
santrix-rc <108999410+santrix-rc@users.noreply.github.com>
2024-09-16 17:45:58 -0500

    Módulo que define la clase Medico

--- app/medico.py
+++ app/medico.py
@@ -1,1 +1,9 @@
+# Autor: Santiago
+class Medico:
+    def __init__(self, nombre, fechas_disponibles, horarios):
+        self.nombre = nombre
+        self.fechas_disponibles = fechas_disponibles
+        self.horarios = horarios
 
+    def __str__(self):
+        return self.nombre
