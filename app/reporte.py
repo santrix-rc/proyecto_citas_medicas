0453649c1302495586add04fa5815d18297d151a
santrix-rc <108999410+santrix-rc@users.noreply.github.com>
2024-09-16 17:48:18 -0500

    Módulo que genera el reporte en formato JSON

--- app/reporte.py
+++ app/reporte.py
@@ -1,1 +1,9 @@
 
+import json
+
+class Reporte:
+    @staticmethod
+    def generar_reporte_json(citas):
+        reporte = [{"paciente": cita.paciente, "medico": cita.medico, "fecha": cita.fecha, "hora": cita.hora}
+                   for cita in citas]
+        return json.dumps(reporte, indent=4)
