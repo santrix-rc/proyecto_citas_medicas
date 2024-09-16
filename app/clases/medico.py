f6069db1f191fce36ed5df2b1dec5b6d3397164b
santrix-rc <108999410+santrix-rc@users.noreply.github.com>
2024-09-16 18:29:10 -0500

    Update medico.py

--- app/clases/medico.py
+++ app/clases/medico.py
@@ -1,9 +1,11 @@
-# Autor: Santiago
 class Medico:
-    def __init__(self, nombre, especialidad):
+    def __init__(self, nombre, especialidades=None, disponibilidad=None):
         self.nombre = nombre
-        self.especialidad = especialidad
-        self.horarios = {}
+        self.especialidades = especialidades if especialidades else []  # Lista de especialidades
+        self.disponibilidad = disponibilidad if disponibilidad else []  # Lista de objetos de Disponibilidad
 
-    def agregar_horario(self, fecha, horas):
-        self.horarios[fecha] = horas
+    def agregar_especialidad(self, especialidad):
+        self.especialidades.append(especialidad)
+
+    def agregar_disponibilidad(self, fecha, horas):
+        self.disponibilidad.append(Disponibilidad(self.nombre, fecha, horas))
