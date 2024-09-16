14399b4f0e81dd3fc92fdac47b8e31c6c031bf7a
santrix-rc <108999410+santrix-rc@users.noreply.github.com>
2024-09-16 18:05:33 -0500

    Update reporte.py

--- app/reporte.py
+++ app/reporte.py
@@ -1,9 +1,10 @@
-
 import json
 
 class Reporte:
-    @staticmethod
-    def generar_reporte_json(citas):
+    def __init__(self, citas):
+        self.citas = citas
+
+    def generar_reporte_json(self):
         reporte = [{"paciente": cita.paciente, "medico": cita.medico, "fecha": cita.fecha, "hora": cita.hora}
-                   for cita in citas]
+                   for cita in self.citas]
         return json.dumps(reporte, indent=4)
