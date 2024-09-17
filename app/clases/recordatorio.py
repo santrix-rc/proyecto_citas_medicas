aeebb07b46c3b9a106c8fa8476a87b0b89b390d6
santrix-rc <108999410+santrix-rc@users.noreply.github.com>
2024-09-16 19:07:52 -0500

    Update recordatorio.py

--- app/clases/recordatorio.py
+++ app/clases/recordatorio.py
@@ -1,8 +1,7 @@
-
 class Recordatorio:
     def __init__(self, cita, mensaje):
         self.cita = cita
         self.mensaje = mensaje
 
     def enviar(self):
-        return f"Recordatorio: {self.mensaje} para la cita de {self.cita.paciente} con {self.cita.medico} el {self.cita.fecha} a las {self.cita.hora}."
+        print(f"Recordatorio enviado a {self.cita.paciente}: {self.mensaje}")
