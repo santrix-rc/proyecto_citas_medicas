77b7cc23a5424cce9459d9e8af41a88c0eacdf5e
santrix-rc <108999410+santrix-rc@users.noreply.github.com>
2024-09-16 19:07:37 -0500

    Update especialidad.py

--- app/clases/especialidad.py
+++ app/clases/especialidad.py
@@ -1,10 +1,3 @@
 class Especialidad:
     def __init__(self, nombre):
         self.nombre = nombre
-
-
-class Disponibilidad:
-    def __init__(self, medico, fecha, horas):
-        self.medico = medico
-        self.fecha = fecha
-        self.horas = horas  # Lista de horas disponibles
